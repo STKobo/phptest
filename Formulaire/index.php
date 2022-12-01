a05c0c6780bf09ba8bb46bdef4c3fb7e5d02cd91
Stephane_tcha <stephane@cypheme.com>
2022-12-01 18:47:51 +0100

    identification des valeurs par le name

--- Formulaire/index.php
+++ Formulaire/index.php
@@ -1,5 +1,5 @@
 <form method="POST" action="form.php">
-    <input type="text">
-    <input type="radio">
+    <input type="text" name="login">
+    <input type="radio" name="email">
     <button type="submit">Envoyer</button>
 </form>
