59b59e02f9bcc25c7027d9762afad4d302e86272
Stephane_tcha <stephanetcha@gmail.com>
2023-01-01 18:50:29 +0100

    utiliser le require pour éviter inserer le footer et le header a chaque page

---
+++ PhPhTML/1/contact.php
@@ -0,0 +1,7 @@
+<?php require 'header.php' ; ?>
+
+<h2>Nous contacter</h2>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, velit neque alias nisi magni distinctio totam perspiciatis? A hic tempore pariatur est, iste reprehenderit iusto minima maxime qui ipsam eaque?</p>
+
+<?php require 'footer.php' ; ?>
