184c8895528241ca8e223e642621dd0b8223cd6b
Stephane_tcha <stephanetcha@gmail.com>
2023-01-05 15:21:59 +0100

    exemple vendeur utilisateurs

---
+++ POO/concepts avancés/users/Seller.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'User.php';
+
+class Seller extends User
+{
+    public string $company; 
+    public function __construct(string $name, string $surname, string $company)
+    {
+        $this->name = $name;
+        $this->surname = $surname; 
+        $this->company = $company; 
+    }
+}
