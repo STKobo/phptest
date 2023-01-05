509dfb6b278e379c3eb0bf5c194bd19cfb503ee4
Stephane_tcha <stephanetcha@gmail.com>
2023-01-05 15:02:30 +0100

    exemple avec un concessionnaire de voiture, électrique et essence)

---
+++ POO/concepts avancés/concession voiture/Car.php
@@ -0,0 +1,13 @@
+<?php 
+
+class Car 
+{
+    public float $price; 
+    public string $brand; 
+
+    public function __construct(float $price, string $brand)
+    {
+        $this->price = $price;
+        $this->brand = $brand; 
+    }
+}
