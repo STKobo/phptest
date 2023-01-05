43d03ab544907343b85cd843d2d420bbfdac84e9
Stephane_tcha <stephanetcha@gmail.com>
2023-01-05 14:35:13 +0100

    visualisation de concepts plus avancés

---
+++ POO/concepts avancés/user.php
@@ -0,0 +1,18 @@
+<?php
+
+class User 
+
+{
+    public string $name;
+    public string $surname;
+
+    public function __construct(string $name, string $surname)
+    {
+        $this->name = $name; 
+        $this->surname = $surname; 
+    }
+    public function getDisplayedName(): string 
+    {
+        return $this->name.' '.$this->surname;
+    }
+}
