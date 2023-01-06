7d72b7a7f7908ebc45d4310329f0ad98c9221a77
Stephane_tcha <stephanetcha@gmail.com>
2023-01-06 18:05:29 +0100

    appel de la methode de la classe mère dans la classe fille

--- POO/concepts avancés/users/Seller.php
+++ POO/concepts avancés/users/Seller.php
@@ -6,13 +6,12 @@ class Seller extends User
     public string $company; 
     public function __construct(string $name, string $surname, string $company)
     {
-        $this->name = $name;
-        $this->surname = $surname; 
+        parent ::__construct($name, $surname); 
         $this->company = $company; 
     }
 
     public function getDisplayedName(): string 
     {
-        return $this->name.' '.$this->surname.' ('.$this->company.') ';
+        return parent::getDisplayedName().' ('.$this->company.') ';
     }
 }
