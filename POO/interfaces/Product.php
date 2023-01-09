b98402713a89b7759d68706b169bfdc0cc56f409
Stephane_tcha <stephanetcha@gmail.com>
2023-01-09 11:38:33 +0100

    ajout class company et product

---
+++ POO/interfaces/Product.php
@@ -0,0 +1,13 @@
+<?php 
+
+class Product 
+{
+    public string $title;
+    public string $description;
+
+    public function __construct(string $title, string $description)
+    {
+        $this->title = $title;
+        $this->description = $description;
+    }
+}
