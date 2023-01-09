f1c88190b62358a677b45ad073fdc8891deb8f61
Stephane_tcha <stephanetcha@gmail.com>
2023-01-09 21:57:32 +0100

    ajout de html entities

--- Jeu/jeu.php
+++ Jeu/jeu.php
@@ -15,7 +15,7 @@ require 'header.php';
 
 
 <form action="/jeu.php" method="GET">
-    <input type="number" name="chiffre" placeholder="entre 0 et 1000" value="<?= (int)$_GET['chiffre'] ?>" > 
+    <input type="text" name="chiffre" placeholder="entre 0 et 1000" value="<?= htmlentities($_GET['chiffre']) ?>" > 
     <button type="submit">Deviner</button>
 </form>
 
