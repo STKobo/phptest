322fbae5eaba7f2248fa38cc570dd1058ddadfcf
Stephane_tcha <stephanetcha@gmail.com>
2023-01-09 22:10:49 +0100

    isset

--- Jeu/jeu.php
+++ Jeu/jeu.php
@@ -1,21 +1,22 @@
 <?php 
-
 $aDeviner = 150;
-
 require 'header.php'; 
-
 ?>
-<?php if($_GET['chiffre'] > $aDeviner) : ?>
-    Votre chiffre est trop grand
-<?php elseif ($_GET['chiffre'] < $aDeviner): ?>
-    Votre chiffre est trop petit
-<?php else : ?>
-    Bravo ! Vous avez deviné le chiffre <?= $aDeviner ?>
+
+
+<?php if (isset($_GET['chiffre'])): ?>
+    <?php if($_GET['chiffre'] > $aDeviner) : ?>
+        Votre chiffre est trop grand
+    <?php elseif ($_GET['chiffre'] < $aDeviner): ?>
+        Votre chiffre est trop petit
+    <?php else : ?>
+        Bravo ! Vous avez deviné le chiffre <?= $aDeviner ?>
+    <?php endif ?>
 <?php endif ?>
 
-
 <form action="/jeu.php" method="GET">
-    <input type="text" name="chiffre" placeholder="entre 0 et 1000" value="<?= htmlentities($_GET['chiffre']) ?>" > 
+    <input type="number" name="chiffre" placeholder="entre 0 et 1000" value="<?= htmlentities($_GET['chiffre']) ?>" > 
+    <input type="text" name="demo" value="test">
     <button type="submit">Deviner</button>
 </form>
 
