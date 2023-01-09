2a97f23290abe48c70c2f79907a159f9a651c49a
Stephane_tcha <stephanetcha@gmail.com>
2023-01-09 22:17:01 +0100

    isset $_GET chiffre

--- Jeu/jeu.php
+++ Jeu/jeu.php
@@ -15,7 +15,7 @@ require 'header.php';
 <?php endif ?>
 
 <form action="/jeu.php" method="GET">
-    <input type="number" name="chiffre" placeholder="entre 0 et 1000" value="<?= htmlentities($_GET['chiffre']) ?>" > 
+    <input type="number" name="chiffre" placeholder="entre 0 et 1000" value="<?php if (isset($_GET['chiffre'])) { echo htmlentities($_GET['chiffre']) ; } ?>" > 
     <input type="text" name="demo" value="test">
     <button type="submit">Deviner</button>
 </form>
