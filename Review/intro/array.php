2ae22b5d635a1235046056b113edfceb6ca114bc
Stephane_tcha <stephanetcha@gmail.com>
2023-01-12 14:21:06 +0100

    propagation du tableau

--- Review/intro/array.php
+++ Review/intro/array.php
@@ -1,7 +1,13 @@
 <?php 
 
 $array = [1, 2, 3, 4]; 
+var_dump($array);
 
 $array[] = "Coucou"; 
+var_dump($array);
 
+array_unshift($array, "Bonjour");
+var_dump($array);
+
+$array[10] = "Ni hao";
 var_dump($array);
