b105e10928135bc90e36da59344680ccdbc75a80
Stephane_tcha <stephanetcha@gmail.com>
2023-01-13 19:34:20 +0100

    layout cards

--- Review/intro/list.php
+++ Review/intro/list.php
@@ -1,21 +1,36 @@
-<?php 
+<?php
 
 $users = [
-    ["id" =>111,
-    "name" => "Irene",
-    "age" => 62],
-    ["id" =>1111,
-    "name" => "Logan",
-    "age" => 72],
-    ["id" =>811,
-    "name" => "Tillie",
-    "age" => 71],
-    ["id" =>2221,
-    "name" => "Luke",
-    "age" => 68],
-    ["id" =>1021,
-    "name" => "Alejandro",
-    "age" => 86]
+    [
+        "id" => 111,
+        "name" => "Irene",
+        "age" => 62,
+        "description" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis necessitatibus dignissimos accusantium magnam. Facilis, quasi odio, consectetur porro natus amet impedit ducimus voluptas nam nobis eveniet nisi sint earum quae."
+    ],
+    [
+        "id" => 1111,
+        "name" => "Logan",
+        "age" => 72,
+        "description" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis necessitatibus dignissimos accusantium magnam. Facilis, quasi odio, consectetur porro natus amet impedit ducimus voluptas nam nobis eveniet nisi sint earum quae."
+    ],
+    [
+        "id" => 811,
+        "name" => "Tillie",
+        "age" => 71,
+        "description" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis necessitatibus dignissimos accusantium magnam. Facilis, quasi odio, consectetur porro natus amet impedit ducimus voluptas nam nobis eveniet nisi sint earum quae."
+    ],
+    [
+        "id" => 2221,
+        "name" => "Luke",
+        "age" => 68,
+        "description" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis necessitatibus dignissimos accusantium magnam. Facilis, quasi odio, consectetur porro natus amet impedit ducimus voluptas nam nobis eveniet nisi sint earum quae."
+    ],
+    [
+        "id" => 1021,
+        "name" => "Alejandro",
+        "age" => 86,
+        "description" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis necessitatibus dignissimos accusantium magnam. Facilis, quasi odio, consectetur porro natus amet impedit ducimus voluptas nam nobis eveniet nisi sint earum quae."
+    ]
 
 ];
 
@@ -23,6 +38,7 @@ $users = [
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -30,7 +46,55 @@ $users = [
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Liste d'utilisateurs</title>
 </head>
+
 <body>
-    
+
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
