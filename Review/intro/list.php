ae65a3a12bf662567b8e0072d9025e485d9c12b6
Stephane_tcha <stephanetcha@gmail.com>
2023-01-13 19:21:16 +0100

    list.php description

--- Review/intro/list.php
+++ Review/intro/list.php
@@ -1,7 +1,36 @@
 <?php 
 
 $users = [
-    "id" =>1,
-    "name" => "Michel",
-    "age" => 20
+    ["id" =>111,
+    "name" => "Irene",
+    "age" => 62],
+    ["id" =>1111,
+    "name" => "Logan",
+    "age" => 72],
+    ["id" =>811,
+    "name" => "Tillie",
+    "age" => 71],
+    ["id" =>2221,
+    "name" => "Luke",
+    "age" => 68],
+    ["id" =>1021,
+    "name" => "Alejandro",
+    "age" => 86]
+
 ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Liste d'utilisateurs</title>
+</head>
+<body>
+    
+</body>
+</html>
