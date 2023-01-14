602e6c104a6e7f6c78c0f12637e0b568214c07ef
Stephane_tcha <stephanetcha@gmail.com>
2023-01-14 15:58:57 +0100

    ajout du echo $user[age]

--- Review/intro/list.php
+++ Review/intro/list.php
@@ -55,7 +55,9 @@ $users = [
             foreach($users as $user){ ?>
             <div class="card" style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title"><?php echo $user['name'] ?></h5>
+                    <h5 class="card-title">
+                        <?php echo $user['name']. " - ". $user['age'] ; ?> 
+                    </h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
