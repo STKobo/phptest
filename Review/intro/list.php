0271bd3c6be4c5102e4c1f2cf1ba50b3ac76a512
Stephane_tcha <stephanetcha@gmail.com>
2023-01-15 19:15:01 +0100

    séparation des responsabilités

--- Review/intro/list.php
+++ Review/intro/list.php
@@ -1,39 +1,5 @@
 <?php
-
-$users = [
-    [
-        "id" => 111,
-        "name" => "Irene",
-        "age" => 62,
-        "description" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis necessitatibus dignissimos accusantium magnam. Facilis, quasi odio, consectetur porro natus amet impedit ducimus voluptas nam nobis eveniet nisi sint earum quae."
-    ],
-    [
-        "id" => 1111,
-        "name" => "Logan",
-        "age" => 72,
-        "description" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis necessitatibus dignissimos accusantium magnam. Facilis, quasi odio, consectetur porro natus amet impedit ducimus voluptas nam nobis eveniet nisi sint earum quae."
-    ],
-    [
-        "id" => 811,
-        "name" => "Tillie",
-        "age" => 71,
-        "description" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis necessitatibus dignissimos accusantium magnam. Facilis, quasi odio, consectetur porro natus amet impedit ducimus voluptas nam nobis eveniet nisi sint earum quae."
-    ],
-    [
-        "id" => 2221,
-        "name" => "Luke",
-        "age" => 68,
-        "description" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis necessitatibus dignissimos accusantium magnam. Facilis, quasi odio, consectetur porro natus amet impedit ducimus voluptas nam nobis eveniet nisi sint earum quae."
-    ],
-    [
-        "id" => 1021,
-        "name" => "Alejandro",
-        "age" => 86,
-        "description" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis necessitatibus dignissimos accusantium magnam. Facilis, quasi odio, consectetur porro natus amet impedit ducimus voluptas nam nobis eveniet nisi sint earum quae."
-    ]
-
-];
-
+include 'data/users.php';
 ?>
 
 <!DOCTYPE html>
