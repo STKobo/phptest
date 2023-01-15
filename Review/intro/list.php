7a0000acd59a3a1f1673e5f033250bc55ba5e5db
Stephane_tcha <stephanetcha@gmail.com>
2023-01-15 18:34:39 +0100

    description

--- Review/intro/list.php
+++ Review/intro/list.php
@@ -58,7 +58,7 @@ $users = [
                     <h5 class="card-title">
                         <?php echo $user['name']. " - ". $user['age'] ; ?> 
                     </h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text"><?php echo $user['description']; ?></p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
