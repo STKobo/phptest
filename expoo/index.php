bcd44f7185fd7b4eb0fc01e05bcbd7917531c589
Stephane_tcha <stephanetcha@gmail.com>
2023-01-16 18:46:55 +0100

    encapsulation

--- expoo/index.php
+++ expoo/index.php
@@ -3,9 +3,9 @@
 class User 
 {
     private int $id;
-    public string $name; 
-    public int $age; 
-    public string $description; 
+    private string $name; 
+    private int $age; 
+    private string $description; 
 
     public function __construct(int $id)
     {
@@ -16,6 +16,47 @@ class User
     {
         return $this->id;
     }
+
+   
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    
+    public function setName(string $name): self 
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    
+    public function getAge(): int 
+    {
+        return $this->age;
+    }
+
+    
+    public function setAge(int $age):self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self 
+    {
+        $this->description = $description;
+
+        return $this;
+    }
 }
 
 $user = new User(123);
@@ -23,5 +64,7 @@ $user = new User(123);
 var_dump($user);
 
 echo $user->getId();
+$user->setName('Gad');
+echo $user->getName();
 
 $admin = new User(1);
